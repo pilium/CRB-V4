4ec13cb118b8cf842a891f7c3cf29a122d7b4dda
Igor Myasnikov <igorpilium@gmail.com>
2025-02-06 22:18:58 +0300

    Update hero section

--- components/sections/hero.vue
+++ components/sections/hero.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 	const { data } = await useAsyncData(() => queryCollection('index').first())
+	console.log(data.value)
 </script>
 <template lang="pug">
 	section.section.section--hero.flc
@@ -7,7 +8,10 @@
 			.container
 				.hero__inner
 					.hero__info
-						h1.hero__title {{ data?.title }}
+						h1.hero__title
+							p.hero__subtitle {{ data?.meta?.hero?.subtitle }}
+							span.hero__title {{ data?.meta?.hero?.title }}
+							p.hero__subtitle {{ data?.meta?.hero?.subtitle1 }}
 						.hero__btns
 							a.btn.btn--border(:href="data?.zapis?.to" target="blanc") {{  data?.zapis?.label }}
 							a.btn.btn--border(:href="data?.Gosuslugi?.to" rel="noopener noreferrer" target="blanc") {{ data?.Gosuslugi?.label }}
@@ -23,13 +27,10 @@
 
 		&__title {
 			margin-top: 0;
-			font-size: 2rem;
-			line-height: 30px;
+			font-size: 3rem;
+			line-height: 1.2;
 
-			@media screen and (min-width: 768px) {
-				font-size: 4.5rem;
-				line-height: 1.2;
-			}
+			@media screen and (min-width: 992px) { font-size: 5rem; }
 		}
 
 		&__subtitle {
@@ -40,22 +41,6 @@
 				max-width: 656px;
 				font-size: 2rem;
 			}
-		}
-
-		&__img {
-			display: none;
-
-			img { width: 875px; }
-
-			@media screen and (min-width: 1200px) {
-				position: absolute;
-				right: 0;
-				bottom: 0;
-				display: var(--isImgShow, block);
-				line-height: 0;
-			}
-
-			@media screen and (min-width: 1366px) { right: 7%; }
 		}
 
 		&__btns {
