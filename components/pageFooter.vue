ed45153b872f3c88d091decfe86757d1ca2394a8
Igor Myasnikov <igorpilium@gmail.com>
2025-02-06 21:52:50 +0300

    Add footer

---
+++ components/pageFooter.vue
@@ -0,0 +1,43 @@
+<script setup>
+function setYear() {
+	const date = new Date().getFullYear();
+	return date;
+}
+</script>
+<template lang="pug">
+	footer.footer
+		.container
+			.footer__inner.fc
+				.footer__copyright
+					p.footer__title ©
+						span.footer__year.js-footer__year {{setYear()}}
+						| ГБУ РО "ЦРБ" в Волгодонском районе.
+					p государственное бюджетное учреждение Ростовской области "Центральная районная больница"
+						|  в Волгодонском районе.
+						b  Все права защищены
+				.footer__social
+					<footerSocial/>
+
+</template>
+<style lang="scss" scoped>
+	.footer {
+		padding: 8px 16px 8px 16px;
+		background-color: var(--color-bg-main);
+
+		&__inner {
+			flex-direction: column-reverse;
+			color: var(--color-light);
+
+			@media screen and (min-width: 992px) {
+				flex-direction: row;
+				justify-content: space-between;
+			}
+		}
+
+		&__title { font-weight: bold; }
+		&__year { margin: 0 8px; }
+
+		&__copyright { max-width: 656px; }
+	}
+
+</style>
---
+++ components/sections/contactsFooter.vue
@@ -0,0 +1,120 @@
+<template lang="pug">
+	.contacts-footer
+		.contacts-footer__inner
+			.contacts-footer__main
+				h2.contacts-footer__title Главный корпус
+				.contacts-footer__item
+					.contacts-footer__icon
+						<Icon name="my-icon:location-pin" class="contacts-footer__svg-icon" title="иконка местоположения"/>
+					address(tabindex="0") Ростовская область, Волгодонской район, ст. Романовская, пер. Союзный 97а
+				.contacts-footer__item
+					.contacts-footer__icon
+						<Icon name="my-icon:mail" class="contacts-footer__svg-icon" title="Иконка электронной почты"/>
+					a.link(href="mailto:crbvr@yandex.ru") crbvr@yandex.ru
+				.contacts-footer__item
+					.contacts-footer__icon
+						<Icon name="my-icon:contact-tel" class="contacts-footer__svg-icon" title="Иконка телефона"/>
+					ul.contacts-footer__tels
+						li
+							span Регистратура
+							a.link(href="tel:88639470722") +7 (86394) 7-07-22
+						li
+							span Колл-центр:
+							a.link(href="tel:89282125806") +7 (928) 212-58-06
+							//- a.link(href="tel:8893947042") +7 (86394) 7-07-22
+				.contacts-footer__item
+					.contacts-footer__icon
+						<Icon name="my-icon:calendar-contacts" class="contacts-footer__svg-icon" title="Иконка календаря"/>
+					<headerSchedule/>
+			.contacts-footer__fil
+				h2.contacts-footer__title Амбулатория
+				.contacts-footer__item
+					.contacts-footer__icon
+						<Icon name="my-icon:location-pin" class="contacts-footer__svg-icon" title="иконка местоположения"/>
+					address(tabindex="0") Ростовская область, Волгодонской район, хутор Рябичев, Юбилейная улица, 52
+				.contacts-footer__item
+					.contacts-footer__icon
+						<Icon name="my-icon:mail" class="contacts-footer__svg-icon" title="Иконка электронной почты"/>
+					a.link(href="mailto:rclinic212@yandex.ru") rclinic212@yandex.ru
+				.contacts-footer__item
+					.contacts-footer__icon
+						<Icon name="my-icon:contact-tel" class="contacts-footer__svg-icon" title="Иконка телефона"/>
+					ul.contacts-footer__tels
+						li
+							span Регистратура:
+							a.link(href="tel:88639474780") 8 (86394) 7-47-80
+				.contacts-footer__item
+					.contacts-footer__icon
+						<Icon name="my-icon:calendar-contacts" class="contacts-footer__svg-icon" title="Иконка календаря"/>
+					<headerSchedule/>
+</template>
+
+<style lang="scss" scoped>
+	.contacts-footer {
+		&__inner {
+			display: block;
+			font-size: 0.875rem;
+
+			@media screen and (min-width: 425px) { font-size: 1rem; }
+
+			@media screen and (min-width: 768px) { display: flex; }
+		}
+
+		&__item {
+			display: flex;
+			align-items: center;
+
+			&:not(:last-child) {
+				margin-bottom: 16px;
+			}
+
+			> * {
+				&:not(:last-child) { margin-right: 8px; }
+			}
+		}
+
+		&__title {
+			margin-top: 8px;
+			margin-bottom: 24px;
+		}
+
+		&__icon {
+			display: flex;
+			align-items: center;
+			flex-shrink: 0;
+			justify-content: center;
+			border-radius: 50%;
+			width: 48px;
+			height: 48px;
+			background: var(--color-main);
+		}
+
+		&__svg-icon {
+			width: 20px;
+			height: 20px;
+			fill: var(--color-light);
+		}
+
+		address {
+			margin-bottom: 0;
+		}
+
+		&__tels {
+			margin: 0;
+
+			li {
+				display: flex;
+				justify-content: space-between;
+
+				> * {
+					&:not(:last-child) {
+						margin-right: 8px;
+						margin-bottom: 8px;
+					}
+				}
+			}
+		}
+
+		@media screen and (min-width: 768px) { padding: 0; }
+	}
+</style>
