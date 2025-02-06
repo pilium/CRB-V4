848c36e6d042a74a4fe4ad2e1ad0d887306d41bb
Igor Myasnikov <igorpilium@gmail.com>
2025-02-06 21:53:28 +0300

    Create default.vue

---
+++ layouts/default.vue
@@ -0,0 +1,30 @@
+<template lang="pug">
+	.main-wrapper.f
+		//- <SkipContent/>
+		<pageHeader/>
+		<NuxtPage/>
+		<pageFooter/>
+		//- <BlindPanel/>
+</template>
+
+<!-- <script>
+import Header from '@/components/header/header';
+
+export default {
+	components: {
+		Header,
+		SkipContent: () => import('@/components/skipToMainContent'),
+		BlindPanel: () => import('@/components/blindPanel'),
+		Footer: () => import('@/components/footer/footer'),
+	},
+	head() {
+		const canonical = `https://xn----8sbbffg6bfugcbry7d4i.xn--p1ai${this.$route.path
+			.toLowerCase()
+			.replace(/\/$/, '')}`;
+
+		return {
+			link: [{rel: 'canonical', href: canonical}],
+		};
+	},
+};
+</script> -->
