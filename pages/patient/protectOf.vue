d785fbea32c02867f7d65c54cc7199dd790380bf
Igor Myasnikov <igorpilium@gmail.com>
2025-02-15 20:00:03 +0300

    Update protectOf.vue

--- pages/patient/protectOf.vue
+++ pages/patient/protectOf.vue
@@ -8,34 +8,35 @@
 				template(v-slot:text)
 					.article__text
 						.container
-							h2.article__title В ванной
-								figure.article__figure
-									img(src="/media/protect/1.jpg" alt="Как защитить себя в ванной")
-								figure.article__figure
-									img(src="/media/protect/2.jpg" alt="Как защитить себя в ванной")
-								figure.article__figure
-									img(src="/media/protect/3.jpg" alt="Как защитить себя в ванной")
-								figure.article__figure
-									img(src="/media/protect/4.jpg" alt="Как защитить себя в ванной")
-								figure.article__figure
-									img(src="/media/protect/5.jpg" alt="Как защитить себя в ванной")
-								figure.article__figure
-									img(src="/media/protect/6.jpg" alt="Как защитить себя в ванной")
-								figure.article__figure
-									img(src="/media/protect/7.jpg" alt="Как защитить себя в ванной")
-							h2.article__title От гриппа
-								figure.article__figure
-									img(src="/media/protect/10.jpg" alt="Как защитить от гриппа")
-								figure.article__figure
-									img(src="/media/protect/11.jpg" alt="Как защитить от гриппа")
-								figure.article__figure
-									img(src="/media/protect/12.jpg" alt="Как защитить от гриппа")
-								figure.article__figure
-									img(src="/media/protect/13.jpg" alt="Как защитить от гриппа")
-								figure.article__figure
-									img(src="/media/protect/14.jpg" alt="Как защитить от гриппа")
-								figure.article__figure
-									img(src="/media/protect/15.jpg" alt="Как защитить от гриппа")
+							UTabs(:items="items" class="w-full")
+								template(#account="{ item }")
+									figure.article__figure
+										img(src="/media/protect/1.jpg" alt="Как защитить себя в ванной")
+									figure.article__figure
+										img(src="/media/protect/2.jpg" alt="Как защитить себя в ванной")
+									figure.article__figure
+										img(src="/media/protect/3.jpg" alt="Как защитить себя в ванной")
+									figure.article__figure
+										img(src="/media/protect/4.jpg" alt="Как защитить себя в ванной")
+									figure.article__figure
+										img(src="/media/protect/5.jpg" alt="Как защитить себя в ванной")
+									figure.article__figure
+										img(src="/media/protect/6.jpg" alt="Как защитить себя в ванной")
+									figure.article__figure
+										img(src="/media/protect/7.jpg" alt="Как защитить себя в ванной")
+								template(#password="{ item }")
+									figure.article__figure
+										img(src="/media/protect/10.jpg" alt="Как защитить от гриппа")
+									figure.article__figure
+										img(src="/media/protect/11.jpg" alt="Как защитить от гриппа")
+									figure.article__figure
+										img(src="/media/protect/12.jpg" alt="Как защитить от гриппа")
+									figure.article__figure
+										img(src="/media/protect/13.jpg" alt="Как защитить от гриппа")
+									figure.article__figure
+										img(src="/media/protect/14.jpg" alt="Как защитить от гриппа")
+									figure.article__figure
+										img(src="/media/protect/15.jpg" alt="Как защитить от гриппа")
 </template>
 <script setup>
 
@@ -48,20 +49,13 @@ useSeoMeta({
 const labels = {
 	last: 'Национальный проект здравоохранения',
 }
+
+const items = [{
+  slot: 'account',
+  label: 'В ванной'
+}, {
+  slot: 'password',
+  label: 'От гриппа'
+}]
+
 </script>
-
-<style lang="scss">
-.tabs {
-	&__gallery-image {
-		margin: 16px 0;
-		background-position: center center;
-		background-size: cover;
-		background-repeat: no-repeat;
-
-		&--fix {
-			width: 300px;
-			height: 200px;
-		}
-	}
-}
-</style>
