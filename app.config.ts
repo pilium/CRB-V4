094e0918558fd09b9702f2c99dbf45e56e0efaa0
Igor Myasnikov <igorpilium@gmail.com>
2025-02-22 21:21:59 +0300

    Create app.config.ts

---
+++ app.config.ts
@@ -0,0 +1,14 @@
+export default defineAppConfig({
+	ui: {
+	  modal: {
+		variants: {
+		  fullscreen: {
+			false: {
+				content: 'top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[calc(100vw-2rem)] max-w-xl max-h-[calc(100dvh-2rem)] sm:max-h-[calc(100dvh-4rem)] rounded-[calc(var(--ui-radius)*2)] shadow-lg ring ring-(--ui-border)'
+			}
+		  }
+		}
+	  },
+	}
+})
+
