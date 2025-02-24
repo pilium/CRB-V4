2b412e8fa810dae96f33fff0986d2fb3de28cf09
Igor Myasnikov <igorpilium@gmail.com>
2025-02-24 17:19:29 +0300

    Update appwrite.js

--- utils/appwrite.js
+++ utils/appwrite.js
@@ -1,10 +1,12 @@
-import { Client, Account} from 'appwrite';
+import { Client, Account, Databases, Storage} from 'appwrite';
 
 export const client = new Client();
 
 client
     .setEndpoint('https://cloud.appwrite.io/v1')
-    .setProject('67b9a3910037abe54add'); // Replace with your project ID
+    .setProject('67b9a3910037abe54add');
 
 export const account = new Account(client);
 export { ID } from 'appwrite';
+export const DB = new Databases(client)
+export const storage = new Storage(client)
