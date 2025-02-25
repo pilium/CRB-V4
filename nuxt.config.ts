b1a92408c93b631939c42fdcdd65a183b60ec423
Igor Myasnikov <igorpilium@gmail.com>
2025-02-25 15:25:35 +0300

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+	runtimeConfig: {
+		public: {
+			API_BASE_ID: process.env.APP_WRITE_ID,
+			DB_ID: process.env.APP_DB_ID,
+			DB_TESTIMONIALS: process.env.APP_TESTIMONIALS_COLLECTION,
+			DB_feedback: process.env.APP_FEEDBACK_COLLECTION,
+			DB_STORAGE: process.env.ARR_STORAGE
+		},
+	},
 	css: ['@/theme/ui.css', '@/theme/variables.scss', '@/theme/index.scss'],
 	modules: ['@nuxt/content', '@nuxt/image', '@nuxt/fonts', "@nuxt/scripts", "@nuxt/eslint", '@nuxt/ui', '@pinia/nuxt'],
 	image: {
