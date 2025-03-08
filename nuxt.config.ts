122a835ccb3735d2d083861bf0665ccf7fd650b0
Igor Myasnikov <igorpilium@gmail.com>
2025-03-08 21:11:38 +0300

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-	runtimeConfig: {
-		public: {
-			API_BASE_ID: process.env.APP_WRITE_ID,
-			DB_ID: process.env.APP_DB_ID,
-			DB_TESTIMONIALS: process.env.APP_TESTIMONIALS_COLLECTION,
-			DB_feedback: process.env.APP_FEEDBACK_COLLECTION,
-			DB_STORAGE: process.env.ARR_STORAGE
-		},
-	},
 	css: ['@/theme/ui.css', '@/theme/variables.scss', '@/theme/index.scss'],
 	modules: ['@nuxt/content', '@nuxt/fonts', "@nuxt/eslint", '@nuxt/ui', '@pinia/nuxt'],
 	fonts: {
@@ -23,6 +14,28 @@ export default defineNuxtConfig({
 					prefix: 'my-icon',
 					dir: './assets/icons'
 			},
+		],
+	},
+	head: {
+		"title": 'ЦРБ Волгодонского района',
+		"meta": [
+			{charset: 'utf-8'},
+			{name: 'viewport', content: 'width=device-width, initial-scale=1'},
+			{hid: 'description', name: 'description', content: process.env.npm_package_description || ''},
+			{name: 'referrer', content: 'same-origin'},
+			{name: 'theme-color', content: '#55c8c0'},
+			{name: 'msapplication-TileColor', content: '#603cba'},
+			{name: 'apple-mobile-web-app-capable', content: 'yes'},
+			{name: 'yandex-verification', content: '7c89cd9671c20987'},
+			{name: 'apple-mobile-web-app-status-bar-style', content: 'black'},
+			{name: 'apple-mobile-web-app-title', content: 'ЦРБ Волгодонского района'},
+		],
+		"link": [
+			{rel: 'apple-touch-icon', href: '/apple-touch-icon.png'},
+			{rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png'},
+			{rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png'},
+			{rel: 'manifest', href: '/site.webmanifest'},
+			{rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#5bbad5'},
 		],
 	},
 	nitro: {
