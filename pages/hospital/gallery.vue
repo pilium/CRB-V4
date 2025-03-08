e7290b649ff7ff34b730eea9553db1f98c505bcc
Igor Myasnikov <igorpilium@gmail.com>
2025-03-08 21:11:49 +0300

    Add pages

---
+++ pages/hospital/gallery.vue
@@ -0,0 +1,82 @@
+<template lang="pug">
+	.inner-wrapper
+		<commonBreadcrumbs :labels="labels"/>
+		main.main#content
+			commonArticle
+				template(v-slot:title)
+					h1.article__title Галерея
+				template(v-slot:text)
+					.article__text
+						.container
+							.tabs__gallery
+								figure.tabs__gallery-item(v-for="image in images")
+									img(:src='image' alt="Развитие системы оказания первичной медико-санитарной помощи")
+</template>
+<script setup>
+const images = [
+	'/media/gallery/756393560.jpg',
+	'/media/gallery/769088457.jpg',
+	'/media/gallery/739379080.jpg',
+	'/media/gallery/787161243.jpg',
+	'/media/gallery/733884061.jpg',
+	'/media/gallery/605783613.jpg',
+	'/media/gallery/689417807.jpg',
+	'/media/gallery/718966061.jpg',
+	'/media/gallery/732920343.jpg',
+	'/media/gallery/711062681.jpg',
+	'/media/gallery/605807621.jpg',
+	'/media/gallery/665053308.jpg',
+	'/media/gallery/662101805.jpg',
+	'/media/gallery/529073431.jpg',
+	'/media/gallery/492572588.jpg',
+	'/media/gallery/507281922.jpg',
+	'/media/gallery/495786529.jpg',
+	'/media/gallery/477104107.jpg',
+	'/media/gallery/492559495.jpg',
+	'/media/gallery/439761294.jpg',
+	'/media/gallery/456319313.jpg',
+	'/media/gallery/40746878.jpg',
+	'/media/gallery/237922410.jpg',
+	'/media/gallery/995327686.jpg',
+	'/media/gallery/422967397.jpg',
+	'/media/gallery/979246918.jpg',
+	'/media/gallery/401453987.jpg',
+	'/media/gallery/847642311.jpg',
+	'/media/gallery/168340098.jpg',
+	'/media/gallery/886821255.jpg',
+	'/media/gallery/866972617.jpg',
+	'/media/gallery/233496063.jpg',
+	'/media/gallery/788660044.jpg',
+	'/media/gallery/112412883.jpg',
+	'/media/gallery/809107557.jpg',
+	'/media/gallery/866950540.jpg',
+]
+useSeoMeta({
+  title: 'Галерея',
+  ogTitle: 'Галерея',
+  description: 'Галерея фото ЦРБ волгодонского района',
+  ogDescription: 'Галерея фото ЦРБ волгодонского района'
+})
+
+const labels = {
+	last: 'Галерея',
+}
+</script>
+<style lang="scss" scoped>
+.tabs__gallery {
+	display: flex;
+	flex-wrap: wrap;
+	justify-content: space-between;
+
+	&-item {
+		margin-bottom: 32px;
+		width: 100%;
+
+		img { height: 100%; }
+
+		@media screen and (min-width: 992px) { width: 49%; }
+
+		@media screen and (min-width: 1366px) { width: 24%; }
+	}
+}
+</style>
---
+++ pages/patient/smo.vue
@@ -0,0 +1,71 @@
+<template lang="pug">
+	.inner-wrapper
+		<commonBreadcrumbs :labels="labels"/>
+		main.main#content
+			commonArticle
+				template(v-slot:title)
+					h1.article__title Страховые компании ростовской области
+				template(v-slot:text)
+					.article__text
+						.container
+							commonTable(:tableData="tableData")
+								template(v-slot:tableData)
+									thead
+										tr
+											th(scope="col") СМО
+											th(scope="col") Название
+											th(scope="col") Телефон
+									tbody
+										tr
+											td
+												img(src="/media/smo/smo_maks.png" alt="МАКС-М")
+											td
+												a.link.link--underline(href="https://www.makc.ru/" target="_blank" rel="noopener noreferrer") Ростовский филиал АО "МАКС-М"
+											td
+												a.link.link--underline(href="tel:+88005550003")
+													b 8-800-555-00-03
+										tr
+											td
+												img(src="/media/smo/logo-alfa_strah.png" alt="АльфаСтрахование")
+											td
+												a.link.link--underline(href="https://alfastrahoms.ru/" target="_blank" rel="noopener noreferrer")  ООО «АльфаСтрахование - ОМС»
+											td
+												a.link.link--underline(href="tel:+88005551001")
+													b 8 800 555 10 01
+										tr
+											td
+												img(src="/media/smo/panaceya2.png" alt="Панацея")
+											td
+												a.link.link--underline(href="https://www.mco-panacea.ru/" target="_blank" rel="noopener noreferrer") ООО МСО «Панацея»
+											td
+												a.link.link--underline(href="tel:+88002000868")
+													b 8-800-200-08-68
+										tr
+											td
+												img(src="/media/smo/SOGAZ-Logotype-RUS.png" alt="Страховая компания «СОГАЗ-Мед")
+											td
+												a.link.link--underline(href="https://www.sogaz-med.ru/" target="_blank" rel="noopener noreferrer")  Ростовский филиал АО «Страховая компания «СОГАЗ-Мед»
+											td
+												a.link.link--underline(href="tel:+88001000702")
+													b 8-800-100-07-02
+										tr
+											td
+												img(src="/media/smo/capital.png" alt="ООО «Капитал МС»")
+											td
+												a.link.link--underline(href="https://kapmed.ru/" target="_blank" rel="noopener noreferrer") Филиал ООО «Капитал МС»
+											td
+												a.link.link--underline(href="tel:+88001008102")
+													b 8 800 100-81-02
+</template>
+
+<script setup>
+useSeoMeta({
+  title: 'Перечень страховых компаний',
+  ogTitle: 'Перечень страховых компаний',
+  description: 'Страховые компании ростовской области',
+  ogDescription: 'Страховые компании ростовской области'
+})
+const labels = {
+	last: 'Перечень страховых компаний',
+}
+</script>
